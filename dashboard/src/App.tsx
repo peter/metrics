160cc5093e8cadfd9d093463d9f6e3312c6c92c5
Peter Marklund <peter@marklunds.com>
2023-12-21 19:28:41 +0100

    basic implementation of dashboard where it updates based on websocket messages

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,19 +1,56 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+
+// Used in order to avoid cyclic dependency, see:
+// https://brandoncc.dev/blog/how-to-deal-with-circular-dependencies-in-react-hooks/
+// Alternative solutions:
+// https://dmitripavlutin.com/react-useeffect-infinite-loop/
+function useNoRenderRef(currentValue: any) {
+  const ref = useRef(currentValue)
+  ref.current = currentValue
+  return ref
+}
 
 function App() {
   const useHeroku = Boolean(new URLSearchParams(window.location.search).get('heroku'))
 
+  // API
+  const API_BASE_URL = useHeroku ? "https://metrics-api-server-63ea51367e93.herokuapp.com" : "http://localhost:8080"
+  const API_URL = `${API_BASE_URL}/metrics-dashboard`
+  const [metrics, setMetrics] = useState(null);
+  const metricsNoRenderRef = useNoRenderRef(metrics);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setMetrics(data.metrics))
+      .catch(error => console.error(error));
+  }, [API_URL]);
+  
   // Websocket
   const WS_URL = useHeroku ? "wss://metrics-websocket-server-094fe427096d.herokuapp.com/ws" : "ws://localhost:3001/ws"
   const { lastMessage, readyState } = useWebSocket(WS_URL);
-  const [messageHistory, setMessageHistory] = useState([]);
   useEffect(() => {
-    if (lastMessage !== null) {
-      setMessageHistory((prev: any) => prev.concat(lastMessage));
+    const metrics: any = metricsNoRenderRef.current;
+    if (metrics && lastMessage !== null) {
+      const { metric: { key: key, value: value } } = JSON.parse(lastMessage.data)
+      const metricIndex: any = ((metrics as any) || []).findIndex((m: any) => m.key === key)
+      if (metricIndex >= 0) {
+        const metric: any = metrics[metricIndex]
+        const newMetric = {
+          key,
+          min: value < metric.min ? value : metric.min,
+          max: value > metric.max ? value : metric.max,
+          avg: (metric.avg * metric.count + value) / (metric.count + 1),
+          count: metric.count + 1
+        }
+        // Avoiding mutation
+        const newMetrics = Object.assign([], metrics, {[metricIndex]: newMetric});
+        setMetrics(newMetrics)
+      }
     }
-  }, [lastMessage, setMessageHistory]);
+  }, [lastMessage, metricsNoRenderRef]);
   const connectionStatusPretty = {
     [ReadyState.CONNECTING]: 'Connecting',
     [ReadyState.OPEN]: 'Open',
@@ -22,92 +59,29 @@ function App() {
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
 
-  // API
-  const API_BASE_URL = useHeroku ? "https://metrics-api-server-63ea51367e93.herokuapp.com" : "http://localhost:8080"
-  const API_URL = `${API_BASE_URL}/metrics-dashboard`
-  const [metrics, setMetrics] = useState(null);
-  useEffect(() => {
-    fetch(API_URL)
-      .then(response => response.json())
-      .then(data => setMetrics(data.metrics))
-      .catch(error => console.error(error));
-  }, [API_URL]);
-
-  // DEBUG PRINTOUTS:
-  console.log('pm debug messageHistory', messageHistory);
-  console.log('pm debug metrics', metrics);
-
   return (
     <div className="p-10">
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Metrics Dashboard
       </h1>
 
       {readyState !== ReadyState.OPEN && <span>WebSocket status: {connectionStatusPretty}</span>}
 
-      <ul>
-        {(metrics || []).map((metric: any) => (
-          <li key={metric.key}>
-            <strong>{metric.key}</strong> min={metric.min} max={metric.max} avg={metric.avg} count={metric.count}
-          </li>
-        ))}
-      </ul>
-
       <div className="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
-        <div className="container max-w-6xl px-5 mx-auto my-28">
-          <div className="grid gap-7 sm:grid-cols-2 lg:grid-cols-4">
-            <div className="p-5 bg-white rounded shadow-sm">
-              <div className="text-base text-gray-400 ">Total Sales</div>
-              <div className="flex items-center pt-1">
-                <div className="text-2xl font-bold text-gray-900 ">$9850.90</div>
-                <span className="flex items-center px-2 py-0.5 mx-2 text-sm text-green-600 bg-green-100 rounded-full">
-                              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                  <path d="M18 15L12 9L6 15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                              </svg>
-                              <span>1.8%</span>
-                </span>
+      {(metrics || []).map((metric: any) => (
+          <div key={metric.key} className="m-3 p-10 bg-white rounded shadow-sm">
+            <div className="text-base text-gray-400 "><strong>{metric.key}</strong></div>
+            <div className="items-center pt-5">
+              <div className="text-2xl font-bold text-gray-900 ">
+                {Math.round(metric.avg)}
               </div>
-            </div>
-            <div className="p-5 bg-white rounded shadow-sm">
-              <div className="text-base text-gray-400 ">Net Revenue</div>
-              <div className="flex items-center pt-1">
-                <div className="text-2xl font-bold text-gray-900 ">$7520.50</div>
-                <span className="flex items-center px-2 py-0.5 mx-2 text-sm text-red-600 bg-red-100 rounded-full">
-                              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                  <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                              </svg>
-                              <span>2.5%</span>
-                </span>
-              </div>
-            </div>
-            <div className="p-5 bg-white rounded shadow-sm">
-              <div className="text-base text-gray-400 ">Customers</div>
-              <div className="flex items-center pt-1">
-                <div className="text-2xl font-bold text-gray-900 ">1375</div>
-                <span className="flex items-center px-2 py-0.5 mx-2 text-sm text-green-600 bg-green-100 rounded-full">
-                              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                  <path d="M18 15L12 9L6 15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                              </svg>
-                              <span>5.2%</span>
-                </span>
-              </div>
-            </div>
-            <div className="p-5 bg-white rounded shadow-sm">
-              <div className="text-base text-gray-400 ">MRR</div>
-              <div className="flex items-center pt-1">
-                <div className="text-2xl font-bold text-gray-900 ">$250.00</div>
-                <span className="flex items-center px-2 py-0.5 mx-2 text-sm text-green-600 bg-green-100 rounded-full">
-                              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                  <path d="M18 15L12 9L6 15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                              </svg>
-                              <span>2.2%</span>
-                </span>
+              <div className="mt-2 block text-sm text-green-600 bg-green-100 rounded-full">
+                min={metric.min} max={metric.max} avg={Math.round(metric.avg)} count={metric.count}
               </div>
             </div>
           </div>
-        </div>
-      </div> 
-
+        ))}
+      </div>
     </div>
   );
 }
